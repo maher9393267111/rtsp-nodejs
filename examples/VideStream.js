df46697e8d8d21fd6c53e0cc3585fd9117838d2b
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-05-16 18:10:38 +0300

    add

---
+++ examples/VideStream.js
@@ -0,0 +1,32 @@
+const videoStream = require('../index');
+let express = require('express');
+let path = require('path');
+let app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'videos')));
+
+
+app.get('/', function(req, res, next) {
+    res.sendFile(__dirname+'/index.html');
+});
+
+
+const options = {
+    url : 'rtsp://185.217.90.19:554/11',
+    //'rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4',
+    segmentFolder : __dirname + '/videos',
+    ffmpegOptions : {  //ffmpeg option.
+        // If you have a ffmpeg option you want to add, write it down here.
+        '-hls_time': '30', // If the same option is set, it is possible to change the value.
+        'ultrafast':undefined // single option other than the key-value type, it can be set by setting the value to undefined.
+    },
+}
+const stream = new videoStream(options);
+stream.start();
+
+
+app.listen(3000);
+
+
+
---
+++ src/VideoStream.js
@@ -0,0 +1,63 @@
+const { spawn } = require('child_process');
+const fs = require('fs');
+
+class VideoStream {
+    constructor(options) {
+        this.url = options.url;
+        this.segmentFolder = options.segmentFolder;
+        this.options = ['-rtsp_transport', 'tcp',
+            '-i', this.url,
+            '-c:v', 'libx264',
+            '-preset', 'ultrafast',
+            '-tune', 'zerolatency',
+            '-f', 'hls',
+            '-hls_time', '10',
+            '-hls_list_size', '5',
+            '-hls_flags', 'delete_segments',
+            `${this.segmentFolder}/stream.m3u8`];
+        if(options.ffmpegOptions !== undefined) {
+            this.#addffmpegOptions(options.ffmpegOptions)
+        }
+
+        console.log(this.options);
+    }
+
+    start() {
+        if (!fs.existsSync(this.segmentFolder)) {
+            fs.mkdirSync(this.segmentFolder);
+        }
+        const ffmpeg = spawn('ffmpeg', this.options);
+        ffmpeg.stderr.on('data', (data) => {
+            console.error(`stderr: ${data}`);
+        });
+
+        ffmpeg.on('close', (code) => {
+            console.log(`ffmpeg process closed with code ${code}`);
+        });
+
+        return ffmpeg;
+    }
+
+    dirPath() {
+        return this.segmentFolder();
+    }
+
+    #addffmpegOptions(ffmpegOptions) {
+        for (const key in ffmpegOptions) {
+            if (!this.options.includes(key)) {
+                this.options.push(key.toString());
+                if(ffmpegOptions[key] !== undefined) {
+                    this.options.push(ffmpegOptions[key].toString());
+                }
+            } else {
+                const ffmpegOptionValueIndex = this.options.indexOf(key) + 1;
+                if(ffmpegOptions[key] !== undefined){
+                  this.options[ffmpegOptionValueIndex] = ffmpegOptions[key].toString();
+                }
+            }
+        }
+    }
+}
+
+
+module.exports = VideoStream;
